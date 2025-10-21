3bee7348ea39f2316914a35ee762f6dd97181959
Taiwo <taiwotriumphant@gmail.com>
2025-10-21 13:14:45 +0100

    fixes pushed

--- src/common/constants/baseUrl.tsx
+++ src/common/constants/baseUrl.tsx
@@ -1,4 +1,4 @@
-export const baseURL = 'http://localhost:3000'; // https://moilapp.com
+export const baseURL = 'https://moilapp.com';
 export const baseURL1 = 'https://moil-business-nextjs-landing.vercel.app/'; // https://moilapp.com
 export const workerBaseUrl = "https://candidate.moilapp.com"; // https://candidate.moilapp.com
 export const businessBaseUrl = "https://business.moilapp.com"; // https://business.moilapp.com
